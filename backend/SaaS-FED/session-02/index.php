e0b11be4c9e777d551a63a2975ad5b6c5a66b8e3
benwpsefton <benwpsefton@gmail.com>
2025-02-12 10:04:14 +0800

    further organised folder structure

--- backend/SaaS-FED/session-02/index.php
+++ backend/SaaS-FED/session-02/index.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+<?php
+$name = "Jacques d'Carre";
+$age = 58;
+$job = "Mechanic";
+
+$age++; // $age = $age + 1; $age +=1;
+
+echo "Hello World";
+
+?>
 </body>
+
 </html>
